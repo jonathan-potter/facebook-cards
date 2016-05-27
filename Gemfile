1b8f3e3344511a137f0bc03f3a5930a3dbe713ac
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-05-26 17:21:17 -0700

    adds static url meta tag
    also adds better errors for debugging

--- Gemfile
+++ Gemfile
@@ -1,3 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+
+group :development do
+  gem 'sinatra-reloader'
+  gem "better_errors"
+  gem "binding_of_caller"
+end
--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require "better_errors"
+
+# configure :development do
+#   use BetterErrors::Middleware
+#   BetterErrors.application_root = __dir__
+# end
 
 get '/meta' do
-  erb :application, locals: {metas: params}
+  erb :application, locals: {request_url: request.url, metas: params}
 end
