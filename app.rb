0a9f1f96ffa46b81ad1b326e998fe315789e0e52
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-08-02 16:24:47 -0700

    remove second bounce. do a better setup for facebook

--- app.rb
+++ app.rb
@@ -1,15 +1,11 @@
 require 'sinatra'
 
-def stringified_params
-  params.map do |key, value|
-    [key, value].join('=')
-  end.join('&')
-end
-
-def shared_meta_tags
+def meta_tags
   {
+    # chute
+    'redirect_url'        => params['url'],
+
     # facebook / pinterest
-    'og:url'              => params['url'],
     'og:title'            => params['title'],
     'og:description'      => params['description'],
     'og:site_name'        => params['site_name'],
@@ -25,18 +21,6 @@ def shared_meta_tags
   }
 end
 
-def first_meta_tags
-  meta_tags = shared_meta_tags
-
-  meta_tags['og:url'] = "http://card-bouncer.herokuapp.com/second?#{stringified_params}"
-
-  meta_tags
-end
-
-get '/first' do
-  erb :application, locals: {request_url: request.url, meta_tags: first_meta_tags}
-end
-
-get '/second' do
-  erb :application, locals: {request_url: request.url, meta_tags: shared_meta_tags}
+get '/meta' do
+  erb :application, locals: {request_url: request.url, meta_tags: meta_tags}
 end
