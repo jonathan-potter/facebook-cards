6e873267cd3be13d22c72759ffd6656ad9e6cf80
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-08-02 16:08:46 -0700

    add second bounce for Facebook!
    doing terrible things

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 
-get '/meta' do
-  metaTags = {
+def stringified_params
+  params.map do |key, value|
+    [key, value].join('=')
+  end.join('&')
+end
+
+def shared_meta_tags
+  {
+    # facebook / pinterest
     'og:url'              => params['url'],
     'og:title'            => params['title'],
     'og:description'      => params['description'],
@@ -9,12 +16,27 @@ get '/meta' do
     'og:image'            => params['image'],
     'og:app_id'           => params['app_id'],
 
+    # twitter
     'twitter:card'        => 'summary_large_image',
     'twitter:site'        => params['twitter_user'],
     'twitter:title'       => params['title'],
     'twitter:description' => params['description'],
     'twitter:image'       => params['image'],
   }
+end
 
-  erb :application, locals: {request_url: request.url, metas: metaTags}
+def first_meta_tags
+  meta_tags = shared_meta_tags
+
+  meta_tags['og:url'] = "http://card-bouncer.herokuapp.com/meta?#{stringified_params}"
+
+  meta_tags
+end
+
+get '/first' do
+  erb :application, locals: {request_url: request.url, meta_tags: first_meta_tags}
+end
+
+get '/second' do
+  erb :application, locals: {request_url: request.url, meta_tags: shared_meta_tags}
 end
