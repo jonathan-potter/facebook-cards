3e4ac3b3e0c68bd9af219eb2fa0e7ff18d37021a
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-08-02 14:29:40 -0700

    ruby is not javascript...

--- app.rb
+++ app.rb
@@ -2,18 +2,18 @@ require 'sinatra'
 
 get '/meta' do
   metaTags = {
-    'og:url'              => params.url,
-    'og:title'            => params.title,
-    'og:description'      => params.description,
-    'og:site_name'        => params.site_name,
-    'og:image'            => params.image,
-    'og:app_id'           => params.app_id,
+    'og:url'              => params['url'],
+    'og:title'            => params['title'],
+    'og:description'      => params['description'],
+    'og:site_name'        => params['site_name'],
+    'og:image'            => params['image'],
+    'og:app_id'           => params['app_id'],
 
     'twitter:card'        => 'summary_large_image',
-    'twitter:site'        => params.twitter_user,
-    'twitter:title'       => params.title,
-    'twitter:description' => params.description,
-    'twitter:image'       => params.image,
+    'twitter:site'        => params['twitter_user'],
+    'twitter:title'       => params['title'],
+    'twitter:description' => params['description'],
+    'twitter:image'       => params['image'],
   }
 
   erb :application, locals: {request_url: request.url, metas: metaTags}
